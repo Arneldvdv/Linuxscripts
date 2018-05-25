57c74a9f56bae1ac30e941f937d5526c9218b5c1
Arneldvdv <ae.van.der.veen@st.hanze.nl>
2018-05-25 09:18:27 +0000

    Script

--- fotoscript.sh
+++ fotoscript.sh
@@ -13,28 +13,35 @@ fi
 
 for i in "$dir"/*
 do
-<hier moet ook nog wat komen>
-done
+filename="$i"
+echo $filename
 
-filename="${i##*/}"
-
-creationdate=$(ls -l --time-style='+%d-%m-%y' "$i" | awk '{print $6}')
-weeknum$(date --date="$creationdate")
-
-if [[ $(find"$filename" -mtime +100 -print) ]]; then
+if [[ $(find . -wholename "$filename"] -mtime +100 -print) ]]; then
 
 mkdir "$dir/$num"
 
 fi
 #copyfile
 cp "$i" "$dir/$num/$filename"
+done
+
+#creationdate=$(ls -l --time-style='+%d-%m-%y' "$i" | awk '{print $6}')
+#weeknum$(date --date="$creationdate")
+
+#if [[ $(find"$filename" -mtime +100 -print) ]]; then
+
+#mkdir "$dir/$num"
+
+#fi
+#copyfile
+#cp "$i" "$dir/$num/$filename"
 
 #check hash
-originalhash=$(sudo md5sum "$i" | cut -d " " =f1)
-movedhash=$(sudo md5sum "$dir/$num/$filename" | cut -d " " -f1)
+#originalhash=$(sudo md5sum "$i" | cut -d " " =f1)
+#movedhash=$(sudo md5sum "$dir/$num/$filename" | cut -d " " -f1)
 
 #remove file
-sudo rm "$i" 
+#sudo rm "$i" 
 
 
 
